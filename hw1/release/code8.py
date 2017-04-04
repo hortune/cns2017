d380e5c4c612265601e07d513520f8a4897f9179
hortune <b04611015@ntu.edu.tw>
2017-04-04 06:39:43 -0700

    faster

--- hw1/release/code8.py
+++ hw1/release/code8.py
@@ -1,43 +1,65 @@
 # coding: utf-8
+from pwn import *
+import hashlib
+import time
 
-"""
-Getting Data
-"""
-parameters = [ i.rstrip().split() for i in open('parameters')]
-parameters = [ i for i in parameters if len(i)]
-parameters = [ int(i[2]) for i in parameters if len(i)]
-p,g,a,b,cipher = parameters
-"""        
-Since g_backdoor = g^((n-1)/691829)  restrict the range by fermet theorem
-"""
-mul = g
-power_num =0
-for i in range(1,691829):
-    mul %= p
-    if mul == a:
-        power_num = i
-        break
-    mul *= g
+p = 262603487816194488181258352326988232210376591996146252542919605878805005469693782312718749915099841408908446760404481236646436295067318626356598442952156854984209550714670817589388406059285064542905718710475775121565983586780136825600264380868770029680925618588391997934473191054590812256197806034618157751903
+            
+nsd = [str(pow(int(hashlib.sha512(str(i)).hexdigest(),16),2,p)) for i in range(21)]
 
-"""
-get the small num a
-"""
-
-s = pow(b,power_num,p)
-
-"""
-implement eucliden algorithm
-"""
-
-def xgcd(b, n):
-        x0, x1, y0, y1 = 1, 0, 0, 1
-        while n != 0:
-                q, b, n = b // n, n, b % n
-                x0, x1 = x1, x0 - q * x1
-                y0, y1 = y1, y0 - q * y1
-        return  b, x0, y0
-    
-flag = xgcd(s,p)[1]
-flag = flag * cipher 
-flag %= p
-print "{0:x}".format(flag).decode("hex")
+def guess(num):
+    for index in range(1,21):
+        conn = remote('140.112.31.109',10002)
+        conn1 = remote('140.112.31.109',10002)
+        for qq in range(3): 
+            if qq == num:
+                conn.recvuntil('Server sends: ')
+                b = int(conn.recvline().strip())
+                conn.sendline(nsd[index])
+                
+                conn1.recvuntil('Server sends: ')
+                b1 = int(conn1.recvline().strip())
+                conn1.sendline(nsd[index])
+            else:
+                conn.recvuntil('Server sends: ')
+                conn1.recvuntil('Server sends: ')
+                temp = int(conn.recvline().strip())
+                temp1 = int(conn1.recvline().strip())
+                conn.sendline(str(temp1))
+                conn1.sendline(str(temp))
+        conn.recvuntil('FLAG is: ')
+        cipher = int(conn.recvline().strip())
+        conn1.recvuntil('FLAG is: ')
+        cipher1 = int(conn1.recvline().strip())
+        
+        conn.close()
+        conn1.close()
+        b = int(hashlib.sha512(str(b)).hexdigest(),16)
+        b1 = int(hashlib.sha512(str(b1)).hexdigest(),16)
+        if cipher^cipher1^b^b1 == 0:
+            return index
+def sol(iter1,iter2,iter3):   
+    print "now", iter1,iter2,iter3
+    conn = remote('140.112.31.109',10002)
+    conn.recvuntil('Server sends: ')
+    b0 = int(conn.recvline().strip())
+    conn.sendline(nsd[iter1])
+    conn.recvuntil('Server sends: ')
+    b1 = int(conn.recvline().strip())
+    conn.sendline(nsd[iter2])
+    conn.recvuntil('Server sends: ')
+    b2 = int(conn.recvline().strip())
+    conn.sendline(nsd[iter3])
+    conn.recvuntil('FLAG is: ')
+    cipher = int(conn.recvline().strip())
+    b = [b0,b1,b2]
+    for keys in b:
+        cipher ^= int(hashlib.sha512(str(keys)).hexdigest(), 16)
+    flag = "{0:x}".format(cipher)
+    if len(flag)%2==0:
+        flag = flag.decode("hex")
+        if "BALSN" in flag:
+            print flag
+            exit()
+    conn.close()
+sol(guess(0),guess(1),guess(2))
